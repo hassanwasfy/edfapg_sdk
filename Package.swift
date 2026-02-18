e21be86dce5efad08d2e0c11e20be01fc5fe38b7
Hassan Wasfy <hassanwasfy7@gmail.com>
2026-02-18 15:22:20 +0200

    SPM: publish iOS binary 0.0.1-alpha.2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "edfapg_sdk",
-            url: "https://raw.githubusercontent.com/hassanwasfy/edfapg_sdk/main/ios/0.0.1-alpha.1/edfapg_sdk.xcframework.zip",
+            url: "https://raw.githubusercontent.com/hassanwasfy/edfapg_sdk/main/ios/0.0.1-alpha.2/edfapg_sdk.xcframework.zip",
             checksum: "2413e31567d382f621a5cf70acf00a7eb2038ec469add41061829aa9d0fd13a2"
         ),
     ]
